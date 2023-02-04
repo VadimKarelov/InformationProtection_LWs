89e9a9c82f0bf925168c4e33ec7f9e8b6a097713
VadimKarelov <vadimkarelov02@gmail.com>
2023-02-04 12:35:47 +0500

    clear code

--- LW1/Cipher.cs
+++ LW1/Cipher.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LW1
+﻿namespace LW1
 {
     public static class Cipher
     {
