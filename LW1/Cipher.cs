390c79082b27dae2215ca7f861d7ed4b6f69c90c
VadimKarelov <vadimkarelov02@gmail.com>
2023-02-04 12:27:54 +0500

    lw1 done

---
+++ LW1/Cipher.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LW1
+{
+    public static class Cipher
+    {
+        /// <summary>
+        /// Шифрование строки методом полибианского квадрата
+        /// </summary>        
+        /// <returns>Возвращается True если строка успешно зашифрована</returns>
+        public static bool PolybianSquare(char[,] matrix, string inputLine, out string outputLine)
+        {
+            outputLine = "";
+
+            if (matrix is null || matrix.GetLength(0) == 0 || matrix.GetLength(1) == 0)
+            {
+                return false;
+            }           
+
+            foreach (char symbol in inputLine)
+            {
+                // проходим по столбикам
+                bool isFind = false;
+                for (int j = 0; j < matrix.GetLength(1) && !isFind; j++)
+                {
+                    for (int i = 0; i < matrix.GetLength(0) && !isFind; i++)
+                    {
+                        if (matrix[i, j] == symbol)
+                        {
+                            isFind = true;
+                            // проверка на последнюю строку
+                            if (i + 1 == matrix.GetLength(0))
+                            {
+                                outputLine += matrix[0, j];
+                            }
+                            else
+                            {
+                                outputLine += matrix[i + 1, j];
+                            }
+                        }
+                    }
+                }
+
+                // не найден символ в таблице -> нельзя закодировать
+                if (!isFind)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- LW1/MainWindow.xaml.cs
+++ LW1/MainWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace LW1
         {
             InitializeComponent();
             CreateTBs();
+            SetRandomTable();
         }
 
         private void CreateTBs()
@@ -32,12 +33,74 @@ namespace LW1
             {
                 for (int j = 0; j < 7; j++)
                 {
-                    TextBox tb = new();
+                    TextBox tb = new()
+                    {
+                        Tag = i.ToString() + j.ToString(),
+                    };
 
                     Grid.SetRow(tb, i);
                     Grid.SetColumn(tb, j);
 
                     grid_table.Children.Add(tb);
+                }
+            }
+        }
+
+        private bool MakeTable(out char[,] outTable)
+        {
+            outTable = new char[4, 7];
+            foreach (var item in grid_table.Children)
+            {
+                if (item is TextBox tb)
+                {
+                    if (tb.Text.Length == 1)
+                    {
+                        if (int.TryParse(tb.Tag.ToString()[0].ToString(), out int i))
+                        {
+                            if (int.TryParse(tb.Tag.ToString()[1].ToString(), out int j))
+                            {
+                                outTable[i, j] = tb.Text[0];
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void Encrypt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (MakeTable(out char[,] table) && Cipher.PolybianSquare(table, tb_in.Text, out string outLine))
+            {
+                tb_out.Text = outLine;
+                tb_out.Background = new SolidColorBrush(Colors.LightGreen);
+            }
+            else
+            {
+                tb_out.Background = new SolidColorBrush(Colors.LightCoral);
+            }
+        }
+
+        private void SetRandomTable()
+        {
+            List<char> symbols = "qwertyuiopasdfghjklzxcvbnm".ToList();
+            Random rn = new();
+
+            foreach (var item in grid_table.Children)
+            {
+                if (item is TextBox tb && symbols.Any())
+                {
+                    int ind = rn.Next(0, symbols.Count);
+                    tb.Text = symbols[ind].ToString();
+                    symbols.RemoveAt(ind);
                 }
             }
         }
