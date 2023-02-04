4414ec2f0c65219d9a118d3bb6d507ee2995f552
VadimKarelov <vadimkarelov02@gmail.com>
2023-02-04 11:08:32 +0500

    create simple ui lw1

--- LW1/MainWindow.xaml.cs
+++ LW1/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace InformationProtection_LWs
+namespace LW1
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
