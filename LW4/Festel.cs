17e8cd9f400166256fdd7432a8a1e8a702fc8f58
VadimKarelov <vadimkarelov02@gmail.com>
2023-02-20 14:59:38 +0500

    rewrite with my class

--- LW4/Festel.cs
+++ LW4/Festel.cs
@@ -4,103 +4,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace LW4
 {
     public static class Festel
     {
-        private static List<BitArray> _keys = new();
-        private static List<List<BitArray>> _history = new();
+        private static List<TBNumber> _keys = new();
+        private static List<List<TBNumber>> _history = new();
 
         public static string Encrypt(string text)
         {
 
         }
 
-        /// <summary>
-        /// Returns text in array of BitArray[16] (16 bit size) with even number of elements
-        /// </summary>
-        private static List<BitArray> TransferStringToArray(string text)
+        private static List<TBNumber> TransferStringToArray(string text)
         {
-            // transfer to bytes array (8 bit)
-            Encoding encoding = Encoding.UTF8;
-            byte[] t = encoding.GetBytes(text);
+            List<TBNumber> resNumber = new();
 
-            // transfer to bit array
-            BitArray bitArray = new BitArray(t);
-            List<BitArray> res = new();
-
-            // counter for full array
-            int i = 0;
-            while (i < bitArray.Length)
+            foreach (char c in text)
             {
-                // counter for one BitArray with 16 bits
-                int j = 0;
-                BitArray oneElement = new BitArray(16);
-                while (i < bitArray.Length && j < 16)
-                {
-                    oneElement[j] = bitArray[i];
-                    i++;
-                    j++;
-                }
-                res.Add(oneElement);
+                resNumber.Add(new TBNumber((Int16)c));
             }
 
-            // for even number of elements
-            if (res.Count % 2 != 0)
-            {
-                res.Add(new BitArray(16));
-            }
-
-            return res;
+            return resNumber;
         }
 
-        /// <summary>
-        /// Transfer array of BitArray (Count = 16) to string
-        /// </summary>
-        private static string TransferArrayToString(List<BitArray> arrays)
+        private static string TransferArrayToString(List<TBNumber> number)
         {
-            List<byte> bytes = new();
-
-            // transfer bits to bytes
-            for (int i = 0; i < arrays.Count; i++)
-            {
-                BitArray x1 = new BitArray(8);
-                BitArray x2 = new BitArray(8);
-
-                // copy first byte
-                for (int j = 0; j < x1.Count; j++)
-                {
-                    x1[j] = arrays[i][j];
-                }
-
-                // copy second byte
-                for (int j = 0; j < x2.Count; j++)
-                {
-                    x2[j] = arrays[i][j + x1.Count];
-                }
-
-                // create arrays of byte to copying
-                byte[] y1 = new byte[1];
-                byte[] y2 = new byte[1];
-
-                // copy bits to bytes
-                x1.CopyTo(y1, 0);
-                x2.CopyTo(y2, 0);
-
-                // get first and alone element of arrays
-                bytes.Add(y1[0]);
-                bytes.Add(y2[0]);
-            }
-
-            Encoding encoding = Encoding.UTF8;
-            return encoding.GetString(bytes.ToArray());
+            return number.Select(x => (char)x.Value).ToString();
         }
 
-        private static BitArray F(BitArray number, BitArray key)
+        private static TBNumber F(TBNumber number, TBNumber key)
         {
-            return new BitArray(number.And(key));
+            return new TBNumber(number.Value * key.Value % Int16.MaxValue);
         }
 
         private static void Round(ref BitArray left, ref BitArray right, ref BitArray key)
---
+++ LW4/TBNumber.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LW4
+{
+    /// <summary>
+    /// Represent 2 bytes number (16 bit)
+    /// </summary>
+    internal class TBNumber
+    {
+        public Int16 Value
+        {
+            get
+            {
+                Int16 pow2 = 1;
+                Int16 res = 0;
+                for (Int16 i = 15; i >= 0; i--)
+                {
+                    res += _value[i] ? pow2 : (Int16)0;
+                    pow2 *= 2;
+                }
+                return res;
+            }
+            set
+            {                
+                _value = Convert.ToString(value, 2).ToArray().Select(x => x == '1').ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Standart order of bits - left most valuable
+        /// </summary>
+        private bool[] _value = new bool[16];
+
+        #region constructor
+        public TBNumber()
+        {
+        }
+
+        public TBNumber(Int16 value)
+        {
+            Value = value;
+        }
+
+        public TBNumber(int value)
+        {
+            Value = Convert.ToInt16(value);
+        }
+        #endregion
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
