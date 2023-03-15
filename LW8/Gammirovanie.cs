19b4df307e9f55d8c48f78207f51c2aad3f8ee4f
VadimKarelov <vadim.karelov.02@mail.ru>
2023-03-15 23:39:27 +0500

    write and tested lw8 back
    - init lw8
    - gammirovanie completed
    - tested by one example

---
+++ LW8/Gammirovanie.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq;
+
+namespace LW8
+{
+    public static class Gammirovanie
+    {
+        public static string Gamma
+        {
+            get
+            {
+                return new string(_gamma.Select(x => ALPHABET[x]).ToArray());
+            }
+            set
+            {
+                _gamma = value.ToCharArray().Select(x => ALPHABET.IndexOf(x)).ToArray();
+            }
+        }
+
+        private static readonly string ALPHABET = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNMйцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ1234567890!\"№;%:?*()@#$^&-=_+{}[]\\|/<>.,'`ёЁ~ ";
+
+        private static int[] _gamma;
+
+        static Gammirovanie()
+        {
+            Gamma = "gammaSentense";
+        }
+
+        public static string Encrypt(string text)
+        {
+            string result = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                // Ci = (Pi + Ki) % N
+                result += ALPHABET[(ALPHABET.IndexOf(text[i]) + _gamma[i % _gamma.Length]) % ALPHABET.Length];
+            }
+
+            return result;
+        }
+
+        public static string Decrypt(string text)
+        {
+            string result = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                // Pi = (Ci + N - Ki) % N
+                result += ALPHABET[(ALPHABET.IndexOf(text[i]) + ALPHABET.Length - _gamma[i % _gamma.Length]) % ALPHABET.Length];
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LW8/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LW8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string t1 = Gammirovanie.Encrypt("Go to walk");
+            string t2 = Gammirovanie.Decrypt(t1);
+        }
+    }
+}
