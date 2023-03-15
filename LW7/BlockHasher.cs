6d74e836e3fb6ed14e86d1fec87ab67a715b8bfb
VadimKarelov <vadimkarelov02@gmail.com>
2023-03-15 10:34:29 +0500

    lw7 done

---
+++ LW7/BlockHasher.cs
@@ -0,0 +1,43 @@
+﻿using LW4;
+
+namespace LW7
+{
+    public static class BlockHasher
+    {
+        private static readonly int HASH_SIZE = 16;
+
+        static BlockHasher()
+        {
+            Festel.GenerateKeys();
+        }
+
+        public static string GetHash(string password)
+        {
+            // remove extra symbols by sum of their codes
+            while (password.Length > HASH_SIZE)
+            {
+                string newPassword = "";
+
+                for (int i = 0; i < password.Length; i += 2)
+                {
+                    if (i + 1 < password.Length)
+                    {
+                        newPassword += (char)((int)password[i] + (int)password[i + 1]);
+                    }
+                    else
+                    {
+                        newPassword += password[i];
+                    }
+                }
+
+                password = newPassword;
+            }
+
+            // adding symbols
+            for (int i = password.Length; i < HASH_SIZE; i++)
+                password += (i % 10).ToString();
+
+            return Festel.Encrypt(password);
+        }
+    }
+}
---
+++ LW7/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LW7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Hash_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            tb_hash.Text = BlockHasher.GetHash(tb_password.Text);
+        }
+    }
+}
