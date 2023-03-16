222ff0c5d8722a7abdbeb2f55358f751452da591
VadimKarelov <vadim.karelov.02@mail.ru>
2023-03-16 21:22:26 +0500

    add buisness logic

--- LW8/MainWindow.xaml.cs
+++ LW8/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LW8
 {
@@ -23,9 +11,38 @@ namespace LW8
         public MainWindow()
         {
             InitializeComponent();
+            tb_gamma.Text = Gammirovanie.Gamma;
+        }
 
-            string t1 = Gammirovanie.Encrypt("Go to walk");
-            string t2 = Gammirovanie.Decrypt(t1);
+        private void Encrypt()
+        {
+            tb_encrypted.Text = Gammirovanie.Encrypt(tb_input.Text);
+        }
+
+        private void Decrypt()
+        {
+            tb_decrypted.Text = Gammirovanie.Decrypt(tb_encrypted.Text);
+        }
+
+        private void ChangeGamma()
+        {
+            Gammirovanie.Gamma = tb_gamma.Text;
+            Encrypt(); // will cause event to decrypt
+        }
+
+        private void TextBoxInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Encrypt();
+        }
+
+        private void TextBoxGamma_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ChangeGamma();
+        }
+
+        private void TextBoxEncrypted_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Decrypt();
         }
     }
 }
